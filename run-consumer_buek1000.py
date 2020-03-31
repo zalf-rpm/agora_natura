7910f99ec80023dac4dbd068e6693f567c94cf4b
AnnaHampf <anna.hampf@posteo.de>
2020-03-31 11:53:58 +0200

    Update run-consumer_buek1000.py

--- run-consumer_buek1000.py
+++ run-consumer_buek1000.py
@@ -125,7 +125,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
 
             if not os.path.isfile(path_to_row_file):
                 with open(path_to_row_file, "w") as _:
-                    _.write("CM-count,row,col,Crop,Year,SowDOY,HarvDOY,Cycle-length,Yield-last,LAI-max,TraDef-avg,NDef-avg,crop-sum-nfert,yearly-sum-nleach\n")
+                    _.write("CM-count,row,col,Crop,Year,SowDOY,HarvDOY,Cycle-length,Yield-last,LAI-max,TraDef-avg,NDef-avg,crop-sum-nfert\n")
                     #_.write("CM-count,row,col,yearly-avg-tavg\n")
 
             with open(path_to_row_file, 'a') as _:
@@ -153,8 +153,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
                                     data["LAI-max"],
                                     data["TraDef-avg"],
                                     data["NDef-avg"],
-                                    data["crop-sum-nfert"],
-                                    data["yearly-sum-nleach"]
+                                    data["crop-sum-nfert"]
                                     #data["Stage-harv"]
                                 ]
                                 writer.writerow(row_)
