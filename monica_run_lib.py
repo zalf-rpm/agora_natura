4dc6a38a30996c6821844b52711c75216ed800f6
AnnaHampf <anna.hampf@gmail.com>
2020-07-06 14:09:21 +0200

    Update monica_run_lib.py

--- monica_run_lib.py
+++ monica_run_lib.py
@@ -96,6 +96,7 @@ def create_seed_harvest_geoGrid_interpolator_and_read_data(path_to_csv_file, wor
         "SBee": False,
         "SB": False,
         "CLALF": False
+        "ALF": False
     }
 
     with open(path_to_csv_file) as _:
