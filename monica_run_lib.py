618cc7980b5061beb70f64d6916028781962fa7c
AnnaHampf <anna.hampf@gmail.com>
2020-07-15 15:43:53 +0200

    sowing  and harvest dates for SWR

--- monica_run_lib.py
+++ monica_run_lib.py
@@ -96,7 +96,8 @@ def create_seed_harvest_geoGrid_interpolator_and_read_data(path_to_csv_file, wor
         "SBee": False,
         "SB": False,
         "CLALF": False,
-        "ALF": False
+        "ALF": False,
+        "SWR": True
     }
 
     with open(path_to_csv_file) as _:
