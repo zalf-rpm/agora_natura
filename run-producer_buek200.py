61eb92c7ab94ca2df1190bfd7159135ab9165ca7
AnnaHampf <anna.hampf@gmail.com>
2020-09-11 15:06:35 +0200

    Update run-producer_buek200.py

--- run-producer_buek200.py
+++ run-producer_buek200.py
@@ -111,7 +111,7 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 DATA_GRID_HEIGHT = "germany/dem_1000_gk5.asc" 
 DATA_GRID_SLOPE = "germany/slope_1000_gk5.asc"
 #DATA_GRID_LAND_USE = "germany/CropLandcoverMap_DE2016_resampled_1000.asc"
-DATA_GRID_LAND_USE = "germany/corine2006_1000_gk5.asc"
+DATA_GRID_LAND_USE = "germany/corine2012_1000_gk5.asc"
 DATA_GRID_SOIL = "germany/BUEK200_1000_gk5.asc"
 DATA_GRID_RNFACTOR = "germany/rNfactor_1000_gk5.asc"
 DATA_GRID_ORGRNFACTOR = "germany/orgrNfactor_1000_gk5.asc"
@@ -359,7 +359,7 @@ def run_producer(server = {"server": None, "port": None}, shared_id = None):
                 # check if current grid cell is used for agriculture                
                 if setup["landcover"]:
                     corine_id = corine_gk5_interpolate(sr_gk5, sh_gk5)
-                    if corine_id not in [200, 210, 211, 212, 240, 241, 242, 243, 244]:
+                    if corine_id not in [12]:
                         continue
 
                 rNfactor = rnf_gk5_interpolate(sr_gk5, sh_gk5)
