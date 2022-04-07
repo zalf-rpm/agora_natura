376d04c5089743e6b5ffa5435733345f9b9908c4
clejae <clemens.jaenicke.1@hu-berlin.de>
2022-04-07 13:55:06 +0200

    Prepare setup for Diana

--- run-consumer_buek200.py
+++ run-consumer_buek200.py
@@ -191,7 +191,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
 
     make_dict_nparr = lambda: defaultdict(lambda: np.full((ncols,), -9999, dtype=np.float))
     
-    output_grids_parked = {
+    output_grids = {
      #   "Globrad-sum": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         #"SowDOY": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
         #"HarvDOY": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
@@ -212,7 +212,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
      #   "G-iso": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
      #   "G-mono": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
     }
-    output_grids = {
+    output_grids_parked = {
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
     output_keys = list(output_grids.keys())
